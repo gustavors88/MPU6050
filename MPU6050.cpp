5a3c8d012c45f262fcef41ca0fdd8aa117394e1a
simondlevy <simon.d.levy@gmail.com>
2017-03-05 00:02:10 -0500

    Fixed typos in headers

--- MPU6050.cpp
+++ MPU6050.cpp
@@ -12,12 +12,12 @@
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation, either version 3 of the License, or
    (at your option) any later version.
-   Hackflight is distributed in the hope that it will be useful,
+   MPU6050 is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
    You should have received a copy of the GNU General Public License
-   along with Hackflight.  If not, see <http://www.gnu.org/licenses/>.
+   along with MPU6050.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "MPU6050.h"
